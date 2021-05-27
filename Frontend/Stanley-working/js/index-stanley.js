7c0f63692a383b180838ff53cfe7467793c59ff9
Stanley <81503453+stanleyei@users.noreply.github.com>
2021-05-28 02:08:25 +0800

    loading新增-變更
    html、scss、css、map、js

--- Frontend/Stanley-working/js/index-stanley.js
+++ Frontend/Stanley-working/js/index-stanley.js
@@ -1,25 +1,47 @@
-var swiper = new Swiper(".aboutUsSwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    effect : 'fade',
-    fadeEffect: {
-      crossFade: true,
-    },
-    mousewheel: true,
-    speed:2500,
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: true,
-      waitForTransition: false,
-      pauseOnMouseEnter: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+const section = document.querySelectorAll('section');
+const loading_page = document.querySelector('.loading-page');
+const random = Math.floor(Math.random()*21)+10;
+let count = 0;
+const loading_timer = setInterval(() => {
+  count++;
+  loading.style.width = `${count}%`;
+  loading_percent.innerText = `${count} %`
+
+  if (count >= 100) {
+    loading_page.classList.add('complete');
+    clearInterval(loading_timer);
+    setTimeout(() => {
+      loading_page.remove();
+    }, 1000);
+    
+    section.forEach(e => {
+      e.style.display = 'flex';
+    });
+  }
+}, random);
+
+let swiper = new Swiper(".aboutUsSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
+  mousewheel: true,
+  speed: 2500,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: true,
+    waitForTransition: false,
+    pauseOnMouseEnter: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
