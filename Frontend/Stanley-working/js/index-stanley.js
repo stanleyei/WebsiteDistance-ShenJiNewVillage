dd110dfdc146b118d8d12a5b79d457f710d61d0f
Stanley <81503453+stanleyei@users.noreply.github.com>
2021-05-27 23:04:50 +0800

    關於審計-變更
    html、scss、css、map、js

--- Frontend/Stanley-working/js/index-stanley.js
+++ Frontend/Stanley-working/js/index-stanley.js
@@ -2,6 +2,18 @@ var swiper = new Swiper(".aboutUsSwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
+    effect : 'fade',
+    fadeEffect: {
+      crossFade: true,
+    },
+    mousewheel: true,
+    speed:2500,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: true,
+      waitForTransition: false,
+      pauseOnMouseEnter: true,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
