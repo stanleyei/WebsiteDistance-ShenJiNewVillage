d8357d08b370a0764b2b8e1d65a26bf3f2d13839
Stanley <81503453+stanleyei@users.noreply.github.com>
2021-06-02 01:20:26 +0800

    審計新訊-編輯

--- Frontend/Stanley-working/js/index-stanley.js
+++ Frontend/Stanley-working/js/index-stanley.js
@@ -34,13 +34,31 @@ const swiper = new Swiper(".aboutUsSwiper", {
 });
 
 //審計新訊-切換選擇日期的效果
+const monthList = document.querySelector('#month-list');
+const yearsList = document.querySelector('#years-list');
+const monthData = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
+let dataValue = 1;
+monthData.forEach(data => {
+  monthList.innerHTML += `<button class="month-btn" data-month="${dataValue}">${data}</button>`
+  dataValue++;
+});
+for (let i = 0; i < 5; i++) {
+  yearsList.innerHTML += `<button class="years-btn">${2019 + i}</button>`;
+};
+
 const monthBtns = document.querySelectorAll('.month-btn');
 const yearsBtns = document.querySelectorAll('.years-btn');
+const date = new Date();
+const thisYear = String(date.getFullYear());
+const thisMonth = String(date.getMonth() + 1);
 focusChange(monthBtns);
 focusChange(yearsBtns);
 
 function focusChange(date) {
   date.forEach(btns => {
+    if (btns.textContent === thisYear || btns.dataset.month === thisMonth) {
+      btns.classList.add('focus-change');
+    }
     btns.addEventListener('click', function () {
       date.forEach(btn => {
         btn === this
