d46732bbb970a2fdd2134d568898714fd8e57f6a
Stanley <81503453+stanleyei@users.noreply.github.com>
2021-06-05 22:31:31 +0800

    安裝swiper套件

--- Backend/Lai/group_web/webpack.mix.js
+++ Backend/Lai/group_web/webpack.mix.js
@@ -12,5 +12,4 @@ const mix = require('laravel-mix');
  */
 
 mix.react('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
-   .version();;
+   .sass('resources/sass/app.scss', 'public/css');
